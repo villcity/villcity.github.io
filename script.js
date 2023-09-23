59e786616aa22f4e1a9e492fa50caeb71ad5249b
villcity <145835277+villcity@users.noreply.github.com>
2023-09-23 12:24:23 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+var donate = document.getElementById('donate')
+
+donate.addEventListener('click', function() {
+    window.location = 'https://www.donationalerts.com/r/Slape_tiktok'
+})
+
+var forum = document.getElementById('forum')
+
+forum.addEventListener('click', function() {
+    window.location = 'https://villcity.forum2x2.ru'
+})
